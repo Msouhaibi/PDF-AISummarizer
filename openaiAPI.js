b119ff02af2caab062cbfd9ff7920c049833deea
soufiyane <soufiyane.aitmoulay@gmail.com>
2023-04-24 12:46:43 +0000

    initial commit

---
+++ openaiAPI.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
+const cors = require('cors'); // Import the cors middleware
+
+app.use(cors()); // Use cors middleware to allow all CORS requests
+
+// Define API route
+app.get('/api/summarize', async (req, res) => {
+    try {
+        const { prompt } = req.query; // Extract prompt from query parameter
+
+        if (!prompt) {
+            // Check if prompt is provided
+            return res.status(400).json({
+                error: 'Prompt is required',
+            });
+        }
+
+        // Call OpenAI API
+        const configuration = new Configuration({
+            apiKey: process.env.OPEN_API_KEY,
+        });
+
+        const openai = new OpenAIApi(configuration);
+
+        const comp = await openai.createCompletion({
+            model: 'text-davinci-003',
+            max_tokens: 2048,
+            prompt: `I want you to act like a pdf text summarizer. I will input text from a pdf and your job is to convert it into a useful summary of a few sentences. Do not repeat sentences and make them clear and concise, : "${prompt}"`, // Use prompt from query parameter
+        });
+
+        // Send response with Access-Control-Allow-Origin header set to allow all origins
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(200).json({
+            summary: comp.data.choices[0].text,
+        });
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).json({
+            error: 'Failed to summarize text',
+        });
+    }
+});
+
+// Serve static files for the front-end
+app.use(express.static('public'));
+
+// Start the Express app
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ script.js
@@ -0,0 +1,84 @@
+const PDFJS = window['pdfjs-dist/build/pdf'];
+let pageContent = [];
+let summarizedData = []
+let file;
+
+const loadPdf = async (pdfUrl) => {
+    try {
+        return await PDFJS.getDocument(pdfUrl).promise;
+    } catch (error) {
+        console.error('Error loading PDF:', error);
+        throw new Error('Failed to load PDF');
+    }
+};
+const extractTextFromPdf = async (pdf) => {
+    const numPages = pdf.numPages;
+    for (let i = 1; i <= numPages; i++) {
+        const page = await pdf.getPage(i);
+        const textContent = await page.getTextContent();
+        const pageText = textContent.items.map(item => item.str).join(' ');
+        pageContent.push({
+            "pageNum": i,
+            "content": pageText
+        });
+    }
+    return pageContent;
+};
+// TODO -> FINISH LATER
+const summarizeText = async (prompt) => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/summarize?prompt="${prompt}"`);
+        if (!response.ok) {
+            throw new Error('Failed to get summarization response');
+        }
+        const data = await response.json();
+        summarizedData.push(data.summary)
+        console.log(summarizedData)
+        pageContent.length===summarizedData.length ? localStorage.setItem(file.name,summarizedData) : false;
+    }
+    catch (error) {
+        console.error(error);
+        throw new Error('Failed to summarize text');
+    }
+};
+document.getElementById("pdfInput").addEventListener("change", (event) => {
+     file = event.target.files[0]; // Get the uploaded file
+});
+document.getElementById("summarize").addEventListener("click",(c)=>{
+    c.preventDefault();
+    var reader = new FileReader();
+    if (file) {
+        if(localStorage){
+            if(localStorage.getItem(file.name)===null){
+                reader.onload = function (e) {
+                    var fileData = e.target.result;
+                    loadPdf(fileData)
+                        .then(pdf => {
+                            if (pdf) {
+                                return extractTextFromPdf(pdf);
+                            }
+                        })
+                        .then(pageContent => {
+                            if (pageContent) {
+                                pageContent.map(page => summarizeText(page.content));
+
+                            }
+                        })
+                        .catch(error => {
+                            console.error(error);
+                        });
+                };
+                reader.readAsArrayBuffer(file);
+            }
+            else{
+                console.log(localStorage.getItem(file.name))
+            }
+        }
+        else{
+            // TODO  LATER A fallback IF LOCALSTORAGE IS NOT SUPPORTED
+        }
+    }
+    else {
+        alert('No file selected.');
+    }
+})
